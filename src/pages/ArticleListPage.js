a0997cc16a2865f5e8450530b5b05075963eca08
boraneak <boraneak123@gmail.com>
2024-03-30 16:45:41 +0700

    create & link articles list

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,5 +1,4 @@
 import { useParams } from 'react-router-dom';
-import { Fragment } from 'react';
 import articles from './article-content';
 const ArticleListPage = () => {
   let { articleId } = useParams();
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 const ArticlePage = () => {
   return (
-    <div>
-      <h1>this is article list  page. </h1>
-    </div>
+    <>
+      <h1>articles</h1>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <Link className="article-list-item" to={`/articles/${article.name}`}>
+            <h1>{article.title}</h1>
+            <p>{article.content[0].substring(0, 145)}</p>
+          </Link>
+        </div>
+      ))}
+    </>
   )
 }
 export default ArticlePage;
