5e526fa2542d73c3fabe757c216bde08712677ac
boraneak <boraneak123@gmail.com>
2024-03-30 16:52:35 +0700

    change route name

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
     <nav>
       <ul>
         <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/article-page"}>Articles</Link></li>
+        <li><Link to={"/articles"}>Articles</Link></li>
         {/* <li><Link to={"/article-page/:article-id"}>Article List</Link></li> */}
         <li><Link to={"/about"}>About</Link></li>
       </ul>
