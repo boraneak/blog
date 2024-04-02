12817786d19313a314a2883fe247beb8a7df46de
boraneak <boraneak123@gmail.com>
2024-04-02 20:59:51 +0700

    ceate a 404 page not found

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,8 +1,12 @@
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
 const ArticlesPage = () => {
   let { articleId } = useParams();
   const article = articles.find(article => article.name === articleId);
+  if (!article) {
+    return <NotFoundPage />
+  }
   return (
     <>
       <h1>{article.title}</h1>
