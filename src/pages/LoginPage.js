8bfd79286ea55c7e35995a6c14873c915130a525
Bora NEAK <bora.neak@axivit.com>
2024-04-14 19:52:47 +0700

    login with firebase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,31 +1,48 @@
 import { useState } from "react";
-import axios from 'axios';
+import { Link, Navigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  console.log('email', email);
-  console.log('password', password);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const Login = async () => {
-    await axios.post('/api/login/', {
-      email: email,
-      password: password
-    });
-  }
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      <Navigate to="/articles" />;
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       <div>
         <h3>login</h3>
+        {error && <p className="error">{error}</p>} <br/>
         <label>
           email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            placeholder="your email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           password:
-          <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            placeholder="your password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button onClick={Login}>login</button>
+        <Link to={"/create-account"}>dont have an account?</Link>
       </div>
     </>
-  )
-}
+  );
+};
 export default LoginPage;
